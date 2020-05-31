fe26c659be3371f40d7e4e387d748988a37e27b0
Erick Yudi Fukuda Mifo <65738815+ErickMifo@users.noreply.github.com>
2020-05-31 13:31:35 -0300

    URL changes

--- getPoke.js
+++ getPoke.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 const getPoke = async function getPoke(ID) {
     try{    
-  let response = await fetch('http://localhost:3001/api/products/' + ID);
+  let response = await fetch('http://localhost:3001/pokemons/' + ID);
   let data = await response.json()
  
   return data.pokemon;
