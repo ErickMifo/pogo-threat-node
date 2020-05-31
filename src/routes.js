f8b9c6a3ca220a0a3b420f6948b6200a0dc209f2
Erick Yudi Fukuda Mifo <65738815+ErickMifo@users.noreply.github.com>
2020-05-31 13:32:14 -0300

    URL changes

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,10 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
 
-routes.get('/products', ProductController.index);
-routes.get('/products/:id', ProductController.show);
-routes.post('/products', ProductController.store);
-routes.put('/products/:id', ProductController.update);
-routes.delete('/products/:id', ProductController.destroy);
+routes.get('/pokemons', ProductController.index);
+routes.get('/pokemons/:id', ProductController.show);
+routes.post('/pokemons', ProductController.store);
+routes.put('/pokemons/:id', ProductController.update);
+routes.delete('/pokemons/:id', ProductController.destroy);
 
 module.exports = routes;
