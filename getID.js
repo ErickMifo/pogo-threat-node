07d8f820942a57e3c228d3801e37a5887db3aaf9
Erick Yudi Fukuda Mifo <65738815+ErickMifo@users.noreply.github.com>
2020-06-15 15:12:20 -0300

    removing try and catch

--- getID.js
+++ getID.js
@@ -1,13 +1,11 @@
 const fetch = require('node-fetch');
 
 async function getID(Number) {
-    try{    
+    
   let response = await fetch('http://localhost:3001/pokemons');
   let data = await response.json()
+  
   return data.docs[Number]._id;
-    } catch(err) {
-        console.log(err.msg)
-    }
 }
 
 
