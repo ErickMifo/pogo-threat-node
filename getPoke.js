0e60f4239408ad3946cd89f6ac05e1fe030e78c5
Erick Yudi Fukuda Mifo <65738815+ErickMifo@users.noreply.github.com>
2020-06-15 15:11:37 -0300

    removing try and catch

--- getPoke.js
+++ getPoke.js
@@ -1,14 +1,11 @@
 const fetch = require('node-fetch');
 
 async function getPoke(ID) {
-    try{    
+    
   let response = await fetch(`http://localhost:3001/pokemons/${ID}`);
   let data = await response.json()
  
   return data.pokemon;
-    } catch(err) {
-        console.log(err.msg)
-    }
 }
 
 
